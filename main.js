02218f7db25f26abd2e265541ff487e0b5a382e2
项晓波 <172927583@qq.com>
2017-11-26 17:13:09 +0800

    初始化项目

---
+++ main.js
@@ -0,0 +1,32 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const fs = require('fs');
+
+
+
+
+function auto_sign(driver, user){
+    driver.get('http://183.131.126.57:9001/hyhbju/master/login.aspx');
+    driver.findElement(By.id('uidTextBox')).sendKeys(user.name);
+    driver.findElement(By.id('pwdTextBox')).sendKeys(user.password);
+    driver.findElement(By.id('Button2')).click();
+
+    driver.get('http://183.131.126.57:9001/hyhbju/WorkDay/DayEdit.aspx?isnew=1');
+    driver.findElement(By.id('ctl00_ContentPlaceHolder1_saveButton')).click();
+
+    driver.get('http://183.131.126.57:9001/hyhbju/master/login.aspx?logout=1');
+}
+
+
+let driver = new Builder()
+    .forBrowser('chrome')
+    .build();
+
+fs.readFile('users.json',
+(err,data)=>{
+   let users=JSON.parse(data);
+   users.forEach((user)=>{
+    auto_sign(driver, user);
+   });
+});
+
+driver.close();
